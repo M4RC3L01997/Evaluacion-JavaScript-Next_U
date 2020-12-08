57969c5a34727f1ebae6d00e037354070eee6321
Marcelo <sebastian.tenis.sc@gmail.com>
2020-12-08 11:12:33 -0300

    He asignado los eventos

--- js/app.js
+++ js/app.js
@@ -22,4 +22,16 @@ var  calculadora = (function(){
 		encontrado : false
 	};
 
+//ASIGNAR EVENTOS
+var suscribirEventos = function(){
+    $(st.numeros).click(events.setNumber);
+    $(st.numeros + "," + st.operadores + "," + st.signo + "," + st.punto + "," + st.resetOn + "," + st.igual)
+    .mousedown(events.pressTecla).mouseup(events.soltarTecla);
+
+    $(st.operadores).click(events.pressOperador);
+    $(st.signo).click(events.setNegativo);
+    $(st.resetOn).click(events.reset);
+    $(st.punto).click(events.setPunto);
+    $(st.igual).click(events.operaciones);
+};
 	
