06919c1f9f94c1cc2d1019b0311f29afa559c67a
Marcelo <sebastian.tenis.sc@gmail.com>
2020-12-08 11:04:00 -0300

    el primer commit de mi proyecto

---
+++ js/app.js
@@ -0,0 +1,25 @@
+var  calculadora = (function(){
+
+
+	//DEFINIR VARIABLES
+	var st = {
+
+		numeros : ".tecla",
+		valNumero : "alt",
+		operadores : ".op",
+		operacion : "",
+		pantalla : "#display",
+		signo : "#sign",
+		punto : "#punto",
+		resetOn : "#on",
+		igual : "#igual",
+		reset : "0" ,
+		valor1 : "",
+		valor2 : "",
+		resultado : "",
+		max : 8,
+		setOperacion : false,
+		encontrado : false
+	};
+
+	
